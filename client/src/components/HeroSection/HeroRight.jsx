ebc0c8123706604d4f47dcac4e170863428e99f2
Shahorier Mustak <shahriarmustak94@gmail.com>
2026-02-28 03:17:17 +0600

    (Feat:UI)Update HeroRight component: enlarge background image and adjust layering

--- client/src/components/HeroSection/HeroRight.jsx
+++ client/src/components/HeroSection/HeroRight.jsx
@@ -1,14 +1,29 @@
+import HeroBgImage from "../../assets/heroBg.png";
 import HeroImage from "../../assets/HeroImage.svg";
+import Rectangle from "../../assets/Rectangle.svg";
 export default function HeroRight() {
   return (
     <div className="hidden lg:block relative">
       <img
-        alt="Professional Worker"
-        className="rounded-3xl "
-        src={HeroImage}
-
-
+        src={HeroBgImage}
+        alt="Hero Background"
+        className="absolute -top-10 w-full right-30 object-cover "
       />
+
+      <div>
+        <img
+          alt="Professional Worker"
+          className="rounded-3xl z-1 w-full relative"
+          src={HeroImage}
+        />
+      </div>
+      <div>
+        <img
+          src={Rectangle}
+          alt="Rectangle"
+          className="absolute -bottom-10 right-0 z-2"
+        />
+      </div>
     </div>
   );
 }
